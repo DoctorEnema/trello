eaa0f981f06caa7b31fe87493a7bdeefd9682473
Sahar Davidyan <Davidyan7@gmail.com>
2021-07-14 14:22:59 +0300

    add close btn

--- src/cmps/card/add-date.vue
+++ src/cmps/card/add-date.vue
@@ -1,12 +1,21 @@
 <template>
   <section class="add-to-card">
        <h1>Hello Date</h1>
+       <button @click="resetModals">X</button>
   </section>
 </template>
 
 <script>
 export default {
-
+data(){
+  return{
+  }
+},
+methods:{
+  resetModals(){
+      this.$emit('closeModals');
+  }
+}
 }
 </script>
 
