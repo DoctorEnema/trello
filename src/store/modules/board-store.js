875b560c6e54a523679f044977fd4d9ef453dd04
Meidan Yona <miedan1994@gmail.com>
2021-07-14 14:15:56 +0300

    some board func

--- src/store/modules/board-store.js
+++ src/store/modules/board-store.js
@@ -2,16 +2,46 @@ import { boardService } from "../../services/board-service"
 
 export const boardStore = {
     state: {
-        boards: [],
-       
+        // boards: [],
+        board: null
     },
     getters: {
-      
+        // boards(state) {
+        //     return state.boards
+        // },
+        board(state){
+            return state.board
+        }
     },
     mutations: {
-        
+        setBoards(state, { boards }) {
+            state.boards = boards
+            console.log(boards);
+        },
+        setBoard(state, { board }) {
+            state.board = board
+            console.log(board);
+        }
     },
     actions: {
-        
+        // async loadBoards(context) {
+        //     try {
+        //         const boards = await boardService.query()
+        //         context.commit({ type: 'setBoard', boards })
+        //         return boards
+        //     } catch (err) {
+        //         console.log('Cannot load boards', err);
+        //     }
+        // },
+        async loadBoard(context) {
+            try {
+                const board = await boardService.query()
+                context.commit({ type: 'setBoard', board })
+                return board
+            } catch (err) {
+                console.log('Cannot load board', err);
+            }
+        },
+
     }
 }
