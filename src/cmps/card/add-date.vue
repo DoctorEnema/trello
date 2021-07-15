f40b8043d0e70a763b4b3744fc351cf02093f15d
Sahar Davidyan <Davidyan7@gmail.com>
2021-07-15 11:55:56 +0300

    date

--- src/cmps/card/add-date.vue
+++ src/cmps/card/add-date.vue
@@ -1,35 +1,50 @@
 <template>
-  <section class="add-to-card">
-       <h1>Hello Date</h1>
-     
-    
-       <button @click="resetModals">X</button>
-  </section>
+<section class="add-to-card">
+  <div class="block">
+    <span class="demonstration">Date</span>
+    <el-date-picker ref="dateInput"
+      v-model="value1"
+      type="date"
+      placeholder="Pick a day">
+    </el-date-picker>
+  </div>
+</section>
 </template>
 
 <script>
-
-export default {
-data(){
-  return{
-
-  }
-},
-components:{
-
-},
-computed:{
-
-},
-methods:{
-  resetModals(){
-      this.$emit('closeModals');
+  export default {
+    data() {
+      return {
+        pickerOptions: {
+          disabledDate(time) {
+            return time.getTime() > Date.now();
+          },
+          shortcuts: [{
+            text: 'Today',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: 'Yesterday',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: 'A week ago',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+      };
+    },
+    mounted() {
+    this.$refs.dateInput.focus();
   },
-  
-}
-}
+  };
 </script>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import app from './app.vue'
-// import Element from 'element-ui'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css';
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './styles/styles.scss'
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
