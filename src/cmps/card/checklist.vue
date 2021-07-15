cc24a2a83ac8fabff4cef2ca5375716c39fe1d48
Liran <liran450@gmail.com>
2021-07-15 17:48:49 +0300

    editing checklist

--- src/cmps/card/checklist.vue
+++ src/cmps/card/checklist.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="checklist">
-      <h1>Checklist</h1>
+    <h1>Checklist</h1>
+    {{card.checklists}}
   </section>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    card: Object,
+  },
+};
 </script>
 
 <style>
-
 </style>
