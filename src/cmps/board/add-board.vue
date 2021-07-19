312f1526ae8b184c91f0a3462a7f8597e46c24c3
Meidan Yona <miedan1994@gmail.com>
2021-07-19 23:02:41 +0300

    pick and create board design

--- src/cmps/board/add-board.vue
+++ src/cmps/board/add-board.vue
@@ -1,19 +1,32 @@
 <template>
-  <section class="add-board">
-    <div>
-      <button @click="openCreatBoard">X</button>
-      <input v-model="title" type="text" />
-      <div class="board-covers">
+  <section>
+    <div @click="toggleCreateBoard" class="add-board-container"></div>
+    <div class="add-board">
+      <div
+        :style="{ backgroundImage: 'url(' + imgUrl + ')' }"
+        class="create-board-header"
+      >
+        <input v-model="title" type="text" placeholder="Add board title" />
+        <button @click="toggleCreateBoard"></button>
+      </div>
+      <div class="add-board-covers">
         <button
           v-for="cover in selectedBoard.covers"
           :key="cover.id"
           @click="imgUrl = cover.imgUrl"
         >
-          <img :src="cover.imgUrl" alt="" />
+          <img :src="cover.imgUrl" />
         </button>
       </div>
+
+      <button
+        class="create-board-btn"
+        :class="{ 'disabled-btn': !title }"
+        @click="createBoard"
+      >
+        Create board
+      </button>
     </div>
-    <button @click="creatBoard">Creat board</button>
   </section>
 </template>
 
@@ -21,34 +34,41 @@
 export default {
   data() {
     return {
-      title: '',
-      imgUrl:'https://res.cloudinary.com/davidyan7/image/upload/v1625997002/samples/landscapes/beach-boat.jpg',
-      covers:[
-    {
-        "id" : "c101",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1625997002/samples/landscapes/beach-boat.jpg"
-    },
-    {
-        "id" : "c102",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1625997005/samples/landscapes/nature-mountains.jpg"
-    },
-    {
-        "id" : "c103",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1625997001/samples/landscapes/architecture-signs.jpg"
-    },
-    {
-        "id" : "c104",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1625996999/samples/landscapes/girl-urban-view.jpg"
-    },
-    {
-        "id" : "c105",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1626442129/download-3_o4vbyr.jpg"
-    },
-    {
-        "id" : "c106",
-        "imgUrl" : "https://res.cloudinary.com/davidyan7/image/upload/v1626442129/download-4_enj2yk.jpg"
-    }
-]
+      title: "",
+      imgUrl:
+        "https://res.cloudinary.com/davidyan7/image/upload/v1625997002/samples/landscapes/beach-boat.jpg",
+      covers: [
+        {
+          id: "c101",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1625997002/samples/landscapes/beach-boat.jpg",
+        },
+        {
+          id: "c102",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1625997005/samples/landscapes/nature-mountains.jpg",
+        },
+        {
+          id: "c103",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1625997001/samples/landscapes/architecture-signs.jpg",
+        },
+        {
+          id: "c104",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1625996999/samples/landscapes/girl-urban-view.jpg",
+        },
+        {
+          id: "c105",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1626442129/download-3_o4vbyr.jpg",
+        },
+        {
+          id: "c106",
+          imgUrl:
+            "https://res.cloudinary.com/davidyan7/image/upload/v1626442129/download-4_enj2yk.jpg",
+        },
+      ],
     };
   },
   computed: {
@@ -57,12 +77,12 @@ export default {
     },
   },
   methods: {
-    openCreatBoard() {
-      this.$emit("openCreatBoard");
+    toggleCreateBoard() {
+      this.$emit("toggleCreateBoard");
     },
-    creatBoard() {
+    createBoard() {
       if (!this.title) return;
-      this.$emit("creatBoard", this.title, this.imgUrl);
+      this.$emit("toggleBoard", this.title, this.imgUrl);
     },
   },
 };
