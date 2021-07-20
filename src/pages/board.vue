09a35a5ff47ef278c7343fb745bdc93099092ac5
Meidan Yona <miedan1994@gmail.com>
2021-07-20 13:25:58 +0300

    some design changes, looks better

--- src/pages/board.vue
+++ src/pages/board.vue
@@ -16,19 +16,21 @@
     />
     <div class="board-header">
       <div class="main-header-side">
-        <h2>Electricity~</h2>
+        <button><h2>Electricity~</h2></button>
         <button class="favorite-board"></button>
-        <button
-          class="board-members"
-          v-for="member in board.members"
-          :key="member._id"
-        >
-          <span v-if="member.imgUrl"><img :src="member.imgUrl" /></span>
-          <span v-else>{{ member.fullname }}</span>
-        </button>
+        <div class="board-members">
+          <button
+            class="board-member-header"
+            v-for="member in board.members"
+            :key="member._id"
+          >
+            <span v-if="member.imgUrl"><img :src="member.imgUrl" /></span>
+            <span v-else>{{ member.fullname }}</span>
+          </button>
+        </div>
         <button>Invite</button>
       </div>
-      <button class="show-board-menu" @click="toggleMenu">Show menu</button>
+      <button class="show-board-menu" @click="toggleMenu"> Show menu</button>
       <side-menu
         @boardCoverColor="boardCoverColor"
         @boardCoverImage="boardCoverImage"
@@ -115,11 +117,10 @@ export default {
     },
   },
   methods: {
-    onDragStart() {
-    },
+    onDragStart() {},
     onDragEnd() {
       const board = this.board;
-      this.$store.commit({type:'setBoard', board})
+      this.$store.commit({ type: "setBoard", board });
       this.$store.dispatch({ type: "updateBoard", board });
     },
     // updateGroup(group) {
