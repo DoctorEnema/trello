ad5921bf8ddfbc611137f3bc1df66380a0b74717
Liran <liran450@gmail.com>
2021-07-22 02:33:41 +0300

    added dashboard

--- src/cmps/board/dashboard.vue
+++ src/cmps/board/dashboard.vue
@@ -1,15 +1,27 @@
 <template>
   <section class="dashboard" @click="closeDashboard">
-    <h1>Hello</h1>
+    <button class="close-dashboard"></button>
+    <h1>Dashboard</h1>
+    <div class="charts-container">
+    <chart-bar class="bar-chart"/>
+    <chart-doughnut class="doughnut-chart"/>
+    </div>
   </section>
 </template>
 
 <script>
+// import VueCharts from 'vue-chartjs'
+// import { Bar, Line } from 'vue-chartjs'
+import chartBar from './chart-bar.vue'
+import chartDoughnut from './chart-doughnut.vue'
 export default {
   data() {
     return {};
   },
-  components: {},
+  components: {
+    chartBar,
+    chartDoughnut
+  },
   computed: {
     selectedBoard() {
       return JSON.parse(JSON.stringify(this.$store.getters.selectedBoard));
